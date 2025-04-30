1e308c857b34c565c7f3b03da3bf872717371a2a
DomoMoho <Domo_W@outlook.com>
2025-04-30 01:39:48 -0700

    Initial Push

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # RoseGarden8thWallARStop
-RoseGarden8thWallARStop
+Augmented Reality Tour Stop made for the Muwekma Ohlone Augmented Reality Tour at Santa Clara University. 
+
+This project aims to use augmented reality to envision a more just future by showing what physical markers could exist showcasing their legacy on the land and connect with indigenous traditions of place-based storytelling.
+
+https://scuhci.com/ohlone-ar/
---
+++ app.js
@@ -0,0 +1,27 @@
+// Copyright (c) 2022 8th Wall, Inc.
+//
+// app.js is the main entry point for your 8th Wall app. Code here will execute after head.html
+// is loaded, and before body.html is loaded.
+
+import {namedWayspotComponent} from './named-wayspot'
+AFRAME.registerComponent('named-wayspot', namedWayspotComponent)
+
+import {playVpsAnimationComponent} from './vps-animation'
+AFRAME.registerComponent('play-vps-animation', playVpsAnimationComponent)
+
+import {shadowShaderComponent} from './shadow-shader'
+AFRAME.registerComponent('shadow-shader', shadowShaderComponent)
+
+import {hiderMaterialComponent} from './hider-material'
+AFRAME.registerComponent('hider-material', hiderMaterialComponent)
+
+import {desktopDevelopmentComponent} from './desktop-development'
+AFRAME.registerComponent('desktop-development', desktopDevelopmentComponent)
+
+// Check Location Permissions at beginning of session
+const errorCallback = (error) => {
+  if (error.code === error.PERMISSION_DENIED) {
+    alert('LOCATION PERMISSIONS DENIED. PLEASE ALLOW AND TRY AGAIN.')
+  }
+}
+navigator.geolocation.getCurrentPosition((pos) => {}, errorCallback)
